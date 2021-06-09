b36a2eb3730d903114b0d3cfc04aed8776410f2e
nhminhng <70113620+nhminhng@users.noreply.github.com>
2021-06-09 16:41:51 -0500

    done tut10

--- main.py
+++ main.py
@@ -72,8 +72,6 @@ def draw_text(text, font, text_color, x, y):
 
 def draw_bg():
 	screen.fill(BG_COLOR)
-	pygame.draw.line(screen, RED, (0, 300), (SCREEN_WIDTH, 300))
-
 
 class Soldier(pygame.sprite.Sprite):
 	def __init__(self, char_type, x, y, scale, speed, ammo, grenades):
@@ -123,6 +121,8 @@ class Soldier(pygame.sprite.Sprite):
 		self.image = self.animation_list[self.action][self.frame_index]
 		self.rect = self.image.get_rect()
 		self.rect.center = (x,y)
+		self.width = self.image.get_width()
+		self.height = self.image.get_height()
 		
 
 	def update(self):
@@ -165,9 +165,23 @@ class Soldier(pygame.sprite.Sprite):
 
 
 		#check collision with floor
-		if self.rect.bottom + dy > 300:
-			dy = 300 - self.rect.bottom
-			self.in_air = False
+
+		for tile in world.obstacle_list:
+			#check collision in the x direction
+			if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
+				dx = 0
+			#check for collision in the y direction
+			if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
+				#check if below the ground, i.e jumping
+				if self.vel_y < 0:
+					self.vel_y = 0
+					dy = tile[1].bottom - self.rect.top
+				#check if above the ground, i.e falling
+				elif self.vel_y >= 0:
+					self.vel_y = 0
+					self.in_air = False
+					dy = tile[1].top - self.rect.bottom
+		
 
 		#update rectangle position
 		self.rect.x += dx
@@ -375,6 +389,12 @@ class Bullet(pygame.sprite.Sprite):
 		#check if bullet has gone off screen
 		if self.rect.right < 0 or self.rect.left > SCREEN_WIDTH:
 			self.kill()
+
+		#check for collision with level
+		for tile in world.obstacle_list:
+			if tile[1].colliderect(self.rect):
+				self.kill()
+
 		#check collision with characters
 		for enemy in enemy_group:
 			if pygame.sprite.spritecollide(enemy, bullet_group, False):
@@ -396,16 +416,33 @@ class Grenade(pygame.sprite.Sprite):
 		self.rect = self.image.get_rect()
 		self.rect.center = (x,y)
 		self.direction = direction
+		self.width = self.image.get_width()
+		self.height = self.image.get_height()
 
 	def update(self):
 		self.vel_y += GRAVITY
 		dx = self.speed * self.direction
 		dy = self.vel_y
 
-		#check collision with floor
-		if self.rect.bottom + dy > 300:
-			dy = 300 - self.rect.bottom
-			self.speed = 0
+
+		#check collision with level
+		for tile in world.obstacle_list:
+			#chcek for x direction
+			if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
+				self.direction *= -1
+				dx = self.direction * self.speed
+			#check for y direction
+			if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
+				self.speed = 0
+				#check if below the ground, i.e throwing up
+				if self.vel_y < 0:
+					self.vel_y = 0
+					dy = tile[1].bottom - self.rect.top
+				#check if above the ground, i.e falling
+				elif self.vel_y >= 0:
+					self.vel_y = 0
+					dy = tile[1].top - self.rect.bottom
+		
 		#check collision with walls
 		if self.rect.left + dx < 0 or self.rect.right + dx > SCREEN_WIDTH:
 			self.direction *= -1
